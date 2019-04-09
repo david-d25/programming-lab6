fada846e09e8cbdfd3c481724c785ad67913b0b2
David <david-d25@yandex.ru>
2019-04-09 23:23:53 +0300

    Improved CreatureFactory to generate multiple creatures from one json-array (of json-objects)

--- src/ru/david/room/CreatureFactory.java
+++ src/ru/david/room/CreatureFactory.java
@@ -2,21 +2,14 @@ package ru.david.room;
 
 import ru.david.room.json.*;
 
-public class CreatureFactory {
-    public static Creature makeCreatureFromJSON(String json) throws Exception {
-        JSONEntity entity;
-
-        try {
-            entity = JSONParser.parse(json);
-        } catch (JSONParseException e) {
-            throw new JSONParseException(e.getMessage());
-        }
-
-        if (entity == null)
-            throw new IllegalArgumentException("Требуется json-объект, но получен null");
-
-        JSONObject object = entity.toObject(new IllegalArgumentException("Нужен json-объект, но вместо него " + entity.getTypeName()));
-
+class CreatureFactory {
+    /**
+     * Создаёт экземпляр класса Creature по заданному json-объекту
+     * @param object json-объект, представляющий экземпляр класса Creature
+     * @return экземпляр класса Creature
+     * @throws Exception когда что-то идёт не так
+     */
+    private static Creature generate(JSONObject object) throws Exception {
         JSONEntity x = object.getItem("x");
         JSONEntity y = object.getItem("y");
 
@@ -42,5 +35,43 @@ public class CreatureFactory {
             creature.setName(name.toString(new IllegalArgumentException("Указанное имя должно быть строкой, но это " + name.getTypeName())).getContent());
 
         return creature;
+    }
+
+    /**
+     * Создаёт существа их json-представления. Если получен json-объект, сгенерируется одно существо.
+     * Если получен json-массив объектов, будет прочтён каждый объект внутри массива и возвращён
+     * массив существ, сгенерированных из каждого объекта
+     * @param json json-представление
+     * @return массив существ
+     * @throws Exception если что-то пойдет не так
+     */
+    static Creature[] generate(String json) throws Exception {
+        JSONEntity entity;
+
+        try {
+            entity = JSONParser.parse(json);
+        } catch (JSONParseException e) {
+            throw new JSONParseException(e.getMessage());
+        }
+
+        if (entity == null)
+            throw new IllegalArgumentException("Требуется json-объект, но получен null");
+
+        if (entity.isObject())
+            return new Creature[]{generate(entity.toObject())};
+        else if (entity.isArray()) {
+            JSONArray array = entity.toArray();
+            Creature[] creatures = new Creature[array.size()];
+            for (int i = 0; i < array.size(); i++)
+                creatures[i] = generate(
+                        array.getItem(i).toObject(
+                                new IllegalArgumentException(
+                                        "Все элементы массива должны быть объектами, но элемент с индексом " + i + " имеет тип " + array.getItem(i).getTypeName()
+                                )
+                        )
+                );
+            return creatures;
+        } else
+            throw new IllegalArgumentException("Нужен json-объект или json-массив, но вместо него " + entity.getTypeName());
     }
 }
