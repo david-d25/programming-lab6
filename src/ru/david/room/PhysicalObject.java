c376900feb2dba47b2979e0b28aae88ff7b89848
David <david-d25@yandex.ru>
2019-04-14 16:24:47 +0300

    Added some javadoc to PhysicalObject class

--- src/ru/david/room/PhysicalObject.java
+++ src/ru/david/room/PhysicalObject.java
@@ -2,36 +2,68 @@ package ru.david.room;
 
 import java.io.Serializable;
 
+/**
+ * Сериализуемый физический объект с координатами, шириной и высотой
+ */
 class PhysicalObject implements Serializable {
     private int x, y, width, height;
 
+    /**
+     * Создаёт физический объект с указанными координатами
+     * @param x x-координата
+     * @param y y-координата
+     */
     PhysicalObject(int x, int y) {
         this(x, y, 0, 0);
     }
 
+    /**
+     * Создаёт физический объект с указанными координатами, шириной и высотой
+     * @param x x-координата
+     * @param y y-координата
+     * @param width ширина
+     * @param height высота
+     */
     PhysicalObject(int x, int y, int width, int height) {
         setBounds(x, y, width, height);
     }
 
-    public void setBounds(int x, int y, int width, int height) {
+    /**
+     * Устанавливает координаты, ширину и высоту объекта
+     * @param x x-координата
+     * @param y y-координата
+     * @param width ширина
+     * @param height высота
+     */
+    private void setBounds(int x, int y, int width, int height) {
         setPosition(x, y);
         setSize(width, height);
     }
 
-    public void setPosition(int x, int y) {
+    /**
+     * Устанавливает координаты объекта
+     * @param x x-координата
+     * @param y y-координата
+     */
+    private void setPosition(int x, int y) {
         setX(x);
         setY(y);
     }
 
-    public void setSize(int width, int height) {
+    /**
+     * Устанавливает размер объекта
+     * @param width ширина
+     * @param height высота
+     */
+    private void setSize(int width, int height) {
         setWidth(width);
         setHeight(height);
     }
 
     public int getX() { return x; }
     public int getY() { return y; }
-    public void setX(int x) { this.x = x; }
-    public void setY(int y) { this.y = y; }
+    private void setX(int x) { this.x = x; }
+    private void setY(int y) { this.y = y; }
 
     public int getWidth() { return width; }
     public int getHeight() { return height; }
