8fbd5c6e76a59520c9fece95adfa2fd064ebd1eb
David <david-d25@yandex.ru>
2019-04-14 16:18:47 +0300

    Updated build script

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
-rm App.jar 2> /dev/null;
+rm ServerApp.jar 2> /dev/null;
+rm ClientApp.jar 2> /dev/null;
 rm -r build 2> /dev/null;
 
 mkdir build;
 
-echo "Compiling...";
-javac -sourcepath src -d build src/ru/david/room/Main.java -encoding UTF-8 &&
-
 (
-  echo "Creating jar file...";
-  jar cfm App.jar src/MANIFEST.MF -C build .
+  echo "Compiling client...";
+  javac -sourcepath src -d build src/ru/david/room/Client.java -encoding UTF-8
+) && (
+  echo "Creating client jar file...";
+  jar cfm ClientApp.jar metadata/CLIENT_MANIFEST.MF -C build .
+) && (
+  echo "Compiling server...";
+  javac -sourcepath src -d build src/ru/david/room/server/Server.java -encoding UTF-8
+) && (
+  echo "Creating server jar file...";
+  jar cfm ServerApp.jar metadata/SERVER_MANIFEST.MF -C build .
 )
