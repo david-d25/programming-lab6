42b4845fbf6c369091342dcadf3333eb258d3f87
David <david-d25@yandex.ru>
2019-04-15 21:03:10 +0300

    Some fixes to javadoc building script

--- build-javadoc.sh
+++ build-javadoc.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo "Generating javadoc...";
 javadoc -d javadoc -private -encoding UTF-8 -sourcepath src/ ru.david.room ru.david.room.server ru.david.room.json src/ru/david/room/*.java
